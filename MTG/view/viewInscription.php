bb90ebe1f57f1693af2030d527bdccb8e7adc229
Éric Larivière <eric.lariviere1999@hotmail.com>
2019-11-07 13:49:30 -0500

    no message

--- MTG/view/viewInscription.php
+++ MTG/view/viewInscription.php
@@ -30,7 +30,88 @@
         </div>
     </fieldset>
 </form>
+<script>
+$("#formulaire_validation").validate({ 
+        rules: { 
+            firstName:{
+                required: true,
+                maxlength: 30,
+                minlength: 3,
+                regexp_firstName: true
+            }, 
+            lastName:{
+                required: true,
+                maxlength: 30,
+                minlength: 3,
+                regexp_lastName: true,
+            }, 
+            email:{
+                required: true,
+                maxlength: 100,
+                regexp_email: true,
+            },
+            password:{
+                required: true,
+                maxlength: 50,
+                minlength: 5,
+                regexp_password: true,
+                }, 
+            password_validation:{
+                required: true,
+                equalTo: "#password",
+                }, 
+            },	    	 
+        messages:{	
+                firstName:
+                {
+                    required: 'Un prénom est obligatoire',
+                    maxlength: 'La longueur maximum est de 30 characteres',
+                    minlength: 'La longueur minimum est de 3 characteres',	
+                },
+                lastName:
+                {
+                    required: 'Un nom de famille est obligatoire',
+                    maxlength: 'La longueur maximum est de 30 characteres',	
+                    minlength: 'La longueur minimum est de 3 characteres',	
+                },
+                email:
+                {
+                    required: 'Un email est obligatoire',
+                    maxlength: 'La longueur maximum est de 100 characteres',	
+                },
+                password:
+                {
+                    required: 'Un mot de passe est obligatoire'	,                    
+                    maxlength: 'La longueur maximum est de 50 characteres',	
+                    minlength: 'La longueur minimum est de 5 characteres'
+                },
+                password_validation:
+                {
+                    required: 'Un mot de passe est obligatoire'	,
+                    equalTo : 'Doit être pareil au mot de passe',
+                },
+            } 
+    });
 
+    $.validator.addMethod("regexp_firstName", 
+    function (value, element){
+    return this.optional(element) || /[a-zA-Z]{30}/.test(value);
+    }, 'Exemple de format: Roy');
+    $.validator.addMethod("regexp_lastName", 
+    function (value, element){
+    return this.optional(element) || /[a-zA-Z]{30}/.test(value);
+    }, 'Exemple de format: Luc');
+    $.validator.addMethod("regexp_password", 
+    function (value, element){
+    return this.optional(element) || /.{0,50}/.test(value);
+    }, 'Exemple de format: xxxx12345678');
+    $.validator.addMethod("regexp_email", 
+    function (value, element){
+    return this.optional(element) || /^[\w\.-]+@[a-zA-Z0-9\.-]{2,}\.[a-zA-Z0-9]{2,6}$/.test(value);
+    }, 'Exemple de format: abc@gmail.com');
+
+
+</script>	
 <?php $contenu = ob_get_clean(); ?>
 
 <?php require 'gabarit.php'; ?>
