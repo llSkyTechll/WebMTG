c97aa76e62b114d95745b50eea9a700520d65b55
Éric Larivière <eric.lariviere1999@hotmail.com>
2019-11-21 13:45:27 -0500

    no message

--- MTG/view/viewAccueil.php
+++ MTG/view/viewAccueil.php
@@ -20,7 +20,7 @@
 				<div class = "form-group d-block"> 
 						<img src="<?php echo $enregistrement['picture']; ?>" class="img-fluid" alt="<?php echo $enregistrement['edition']; ?>">
 
-						<button type="button" class="btn btn-primary" value="<?php echo $enregistrement['packid']; ?>">Ajouter au panier</button>
+						<button type="button" class="btn btn-light" value="<?php echo $enregistrement['packid']; ?>">Ajouter au panier</button>
 				</div>
 				
 			<?php }
