a995586f9db2c76dd6ddca26276cc70cccbdbae9
Alexandre Reny <areny1998@hotmail.com>
2019-12-02 18:34:04 -0500

    DatabaseV2

--- MTG/model/ManagerOrders.php
+++ MTG/model/ManagerOrders.php
@@ -3,33 +3,11 @@
 
   class ManagerOrders extends Connexion // hérite de la classe connexion
   {
-    // Renvoie la liste des membres triés par nom dans l'ordre alphabétique
-    public function GetCart($custid) {
-      $sql = 'SELECT cartcontentid, tbl_cartcontent.cartid, tbl_cartcontent.quantity, tbl_packs.packid, edition, picture, description
-              FROM tbl_cartcontent
-              INNER JOIN tbl_cart ON tbl_cart.cartid = tbl_cartcontent.cartid
-              INNER JOIN tbl_packs ON tbl_packs.packid = tbl_cartcontent.packid
-              WHERE custid = '.$custid;
-  	  $cart = self::getConnexion()->prepare($sql);
-      $cart->execute();
-  	  return $cart;
-    }
-    public function AddToCart($custid) {
-      $sql = 'SELECT cartcontentid, tbl_cartcontent.cartid, tbl_cartcontent.quantity, tbl_packs.packid, edition, picture, description
-              FROM tbl_cartcontent
-              INNER JOIN tbl_cart ON tbl_cart.cartid = tbl_cartcontent.cartid
-              INNER JOIN tbl_packs ON tbl_packs.packid = tbl_cartcontent.packid
-              WHERE custid = '.$custid;
-  	  $cart = self::getConnexion()->prepare($sql);
-      $cart->execute();
-  	  return $cart;
-    }
-
     public function GetOrders($custid){
       $sql = 'SELECT *
               FROM tbl_order
-              INNER JOIN tbl_cartcontent ON tbl_cartcontent.cartid = tbl_order.cartid
-              INNER JOIN tbl_packs ON tbl_packs.packid = tbl_cartcontent.packid
+              INNER JOIN tbl_ordercontent ON tbl_ordercontent.orderid = tbl_order.orderid
+              INNER JOIN tbl_packs ON tbl_packs.packid = tbl_ordercontent.packid
               WHERE custid = '.$custid.'
               ORDER BY date DESC';
       $orders = self::getConnexion()->prepare($sql);
