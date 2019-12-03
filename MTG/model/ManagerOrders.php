aef923b1249dd2cbc83b5c24b77538a389936740
Alexandre Reny <areny1998@hotmail.com>
2019-12-03 14:31:40 -0500

    Order by orderid

--- MTG/model/ManagerOrders.php
+++ MTG/model/ManagerOrders.php
@@ -9,13 +9,13 @@
       INNER JOIN tbl_ordercontent ON tbl_ordercontent.orderid = tbl_order.orderid
       INNER JOIN tbl_packs ON tbl_packs.packid = tbl_ordercontent.packid
       WHERE custid = '.$custid.'
-      ORDER BY date DESC';
+      ORDER BY date DESC, orderid DESC';
       $orders = self::getConnexion()->prepare($sql);
       $orders->execute();
       return $orders;
     }
     public function AddOrderContent($customerorder,$selectedpack,$chosenquantity){
-      $sql =  'call customerorder(:customerorder,:selectedpack,:chosenquantity)'; 
+      $sql =  'call customerorder(:customerorder,:selectedpack,:chosenquantity)';
       $OrderContent = self::getConnexion()->prepare($sql);
       $OrderContent->bindparam('customerorder',$customerorder,pdo::PARAM_STR,999);
       $OrderContent->bindparam('selectedpack',$selectedpack,pdo::PARAM_STR,999);
@@ -24,14 +24,14 @@
       return $OrderContent;
     }
     public function CreateOrder($customerid){
-      $sql =  'call CreateOrder(:customerid)'; 
+      $sql =  'call CreateOrder(:customerid)';
       $Customer = self::getConnexion()->prepare($sql);
       $Customer->bindparam('customerid',$customerid,pdo::PARAM_STR,999);
       $Customer->execute();
       return $Customer;
     }
     public function UpdateOrder($customerid){
-      $sql =  'call UpdateOrder(:customerid)'; 
+      $sql =  'call UpdateOrder(:customerid)';
       $UpdateOrder = self::getConnexion()->prepare($sql);
       $UpdateOrder->bindparam('customerid',$customerid,pdo::PARAM_STR,999);
       $UpdateOrder->execute();
